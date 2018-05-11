b109bab9afe8cbf66cd3cdccf99649e91f8a97ef
david <ariedov@gmail.com>
2018-05-12 01:17:56 +0300

    adds some simple methods to paragraph

--- lib/paragraph.dart
+++ lib/paragraph.dart
@@ -14,31 +14,47 @@ class PrayerParagraph extends StatefulWidget {
 }
 
 class ParagraphState extends State<PrayerParagraph> {
+  bool _showRussian;
+  bool _showHebrew;
+
+  void showRussian() {
+    setState(() {
+      _showRussian = true;
+      _showHebrew = false;
+    });
+  }
+
+  void showHebrew() {
+    setState(() {
+      _showRussian = false;
+      _showHebrew = true;
+    });
+  }
+
+  void hideTranslations() {
+    setState(() {
+      _showRussian = false;
+      _showHebrew = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Stack(
       children: <Widget>[
         new Opacity(
-            opacity: 0.0,
+            opacity: _showRussian ? 1.0 : 0.0,
             child: new Container(
                 decoration: new BoxDecoration(color: Colors.white),
-                padding: new EdgeInsets.only(right: 10.0),
-                child: new Text(widget.russian)
-            )
-        ),
+                margin: new EdgeInsets.only(right: 10.0),
+                child: new Text(widget.russian))),
+        new Opacity(opacity: 1.0, child: new Text(widget.transliteration)),
         new Opacity(
-            opacity: 1.0,
-            child: new Text(widget.transliteration)
-        ),
-        new Opacity(
-            opacity: 0.0,
+            opacity: _showHebrew ? 1.0 : 0.0,
             child: new Container(
                 decoration: new BoxDecoration(color: Colors.white),
-                padding: new EdgeInsets.only(left: 10.0),
-                child: new Text(widget.hebrew)
-            )
-        )
+                margin: new EdgeInsets.only(left: 10.0),
+                child: new Text(widget.hebrew)))
       ],
     );
   }
